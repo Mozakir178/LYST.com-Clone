99c8384df4e2a531f4e79bd8f42c9220dbc633d8
Pooja_K <kanadepooja789@gmail.com>
2022-07-21 08:24:21 +0530

    fetch

---
+++ fetchappend/fetchp.js
@@ -0,0 +1,17 @@
+
+const getData = async(url)=>{
+    try{
+        let res = await fetch(url);
+        let data = res.json();
+        console.log(data);
+    }
+    catch(err){
+        console.log("err",err);
+    }
+}
+
+export {getData};
+// const appendData = (data,bmain)=>{
+//     bmain.innerHTML = null;
+//     data.forEach
+// }
