bb0f720bc8a48f163e0d1dfff64a88f41b8d3d26
Pooja_K <kanadepooja789@gmail.com>
2022-07-22 14:01:30 +0530

    brows done

--- fetchappend/fetchp.js
+++ fetchappend/fetchp.js
@@ -3,7 +3,10 @@ const getData = async(url)=>{
     try{
         let res = await fetch(url);
         let data =await  res.json();
-        let Data=data.BrowswAndShop
+        let Data=data.BrowswAndShop 
+        if(!Data){
+            Data = data.Trendinspire
+        }
         console.log(Data);
          return Data;
     }
@@ -21,14 +24,45 @@ const appendData = (Data,bmain)=>{
         let img = document.createElement("img");
         img.src = ele.image;
 
-        let Title = document.createElement("h4");
+        let Title = document.createElement("p");
         Title.innerText = ele.title;
+        Title.setAttribute('class','productTitle')
 
         let Detail = document.createElement("p");
         Detail.innerText = ele.detail;
+        Detail.setAttribute("class",'productDetail')
 
-        div.append(img,Title,Detail);
+        let shopbtn = document.createElement('button');
+        shopbtn.innerHTML = `Shop Now   <i class="fa-solid fa-angle-right"></i>`
+        shopbtn.setAttribute("id",'shopBtn')
+
+        div.append(img,Title,Detail,shopbtn);
         bmain.append(div);
     })
 }
-export {getData, appendData}
+
+const appendTrending = (Data,trending)=>{
+    trending.innerHTML = null;
+    Data.forEach((ele)=>{
+        let div = document.createElement("div");
+       
+        let img = document.createElement("img");
+        img.src = ele.image;
+
+        let Title = document.createElement("p");
+        Title.innerText = ele.title;
+        Title.setAttribute('class','productTitle')
+
+        let Detail = document.createElement("p");
+        Detail.innerText = ele.detail;
+        Detail.setAttribute("class",'productDetail')
+
+        let shopbtn = document.createElement('button');
+        shopbtn.innerHTML = `Shop Now   <i class="fa-solid fa-angle-right"></i>`
+        shopbtn.setAttribute("id",'shopBtn')
+
+        div.append(img,Title,Detail,shopbtn);
+        trending.append(div);
+    })
+}
+export {getData, appendData, appendTrending}
